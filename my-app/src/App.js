27eb2d04b9fb1ce223efa1158ed9a85691fb23d5
Joanna <joanna.fornal@rabobank.nl>
2021-02-24 13:38:28 +0100

    adding button, fetch data on click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,44 @@
-import {useEffect, useState } from 'react'
-import { Card, CardContent, CssBaseline, Container, Typography } from '@material-ui/core';
+import { useState } from 'react'
+import { Button, Box, Card, CardContent, CssBaseline, Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  card: {
+    marginBottom: 20
+  },
+  cardContent: {
+    padding: '1rem'
+  },
+  box: {
+    marginBottom: 20
+  }
+})
 
 function App() {
   const [jokes, setJokes] = useState([])
-  useEffect(() => {
+  const classes = useStyles();
+  
+const fetchData = () => {
     fetch('http://api.icndb.com/jokes/random/10')
-    .then(res => res.json())
-    .then(res => setJokes(res.value))
-    .catch((err) => console.log(err))
-  }, [])
+      .then(res => res.json())
+      .then(res => setJokes(res.value))
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
-      <CssBaseline/>
+      <CssBaseline />
       <Container>
         <Typography variant='h1' align='center'>
           Chuck Norris Jokes App
         </Typography>
+        <Box display='flex' justifyContent='center' className={classes.box}>
+          <Button variant='outlined' color='primary' onClick={fetchData}>Fetch data</Button>
+        </Box>
+
         {jokes.map(joke => (
-          <Card key={joke.id}>
-            <CardContent>
+          <Card key={joke.id} className={classes.card}>
+            <CardContent className={classes.cardContent}>
               <Typography>{joke.joke}</Typography>
             </CardContent>
           </Card>
