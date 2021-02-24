9af8d8bb2200a529280df2b1efa67a2687ee2875
Joanna <joanna.fornal@rabobank.nl>
2021-02-24 12:50:22 +0100

    building a base structure and retrieve first data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,29 @@
-
+import {useEffect, useState } from 'react'
+import { Card, CardContent, CssBaseline, Container, Typography } from '@material-ui/core';
 
 function App() {
+  const [jokes, setJokes] = useState([])
+  useEffect(() => {
+    fetch('http://api.icndb.com/jokes/random/10')
+    .then(res => res.json())
+    .then(res => setJokes(res.value))
+    .catch((err) => console.log(err))
+  }, [])
   return (
     <div className="App">
+      <CssBaseline/>
+      <Container>
+        <Typography variant='h1' align='center'>
+          Chuck Norris Jokes App
+        </Typography>
+        {jokes.map(joke => (
+          <Card key={joke.id}>
+            <CardContent>
+              <Typography>{joke.joke}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Container>
     </div>
   );
 }
