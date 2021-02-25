203850dcb55db75853e45238a79a5317ac7419ab
Joanna <joanna.fornal@rabobank.nl>
2021-02-25 13:47:49 +0100

    little clean up and adding comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { AppBar, Button, Box, Card, CardActions, CardContent, CssBaseline, Container, Tab, Tabs, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -7,86 +7,106 @@ const useStyles = makeStyles({
     marginBottom: 20,
     position: 'sticky'
   },
+  box: {
+    marginBottom: 20
+  },
   card: {
     marginBottom: 20
   },
   cardContent: {
     padding: '1rem'
-  },
-  box: {
-    marginBottom: 20
   }
 })
 
 function App() {
-  const initialFavouriteJokes = () =>  JSON.parse(window.localStorage.getItem('favouriteJokes')) || []
-  const [jokes, setJokes] = useState([])
-  const [favouriteJokes, setFavouriteJokes] = useState(initialFavouriteJokes)
-  const [currentTab, setCurrentTab] = useState(0)
+  const initialFavouriteJokes = () =>  JSON.parse(window.localStorage.getItem('favouriteJokes')) || []  
   const [buttonValue, setButtonValue] = useState(false)
-  const [intervalId, setIntervalId] = useState(null)
   const [changeInInterval, setChangeInInterval] = useState(false)
+  const [currentTab, setCurrentTab] = useState(0)
+  const [favouriteJokes, setFavouriteJokes] = useState(initialFavouriteJokes)
+  const [intervalId, setIntervalId] = useState(null)
+  const [jokes, setJokes] = useState([])
   const classes = useStyles();
   const stateRef = useRef();
   const toggle = useRef()
   toggle.current = buttonValue;
   stateRef.current = favouriteJokes;
 
+  const changeTabs = (event, value) => {
+    setCurrentTab(value);
+  }
+
   const fetchData = () => {
+    // fetches 10 jokes and sets them into joke state
     fetch('http://api.icndb.com/jokes/random/10')
       .then(res => res.json())
       .then(res => setJokes(res.value))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
+  }
+
+  const fetchRandomJokes = () => {
+    // triggers change in button value and will call useEffect to fetch random jokes
+    setButtonValue(!toggle.current);
+  }
+
+  const setFavourite = (id) => {
+    // checks if no more than 10 jokes or if jokes already exists
+    if(favouriteJokes.length > 9 || favouriteJokes.find((joke) => joke.id === id)) return;
+    // finds the joke
+    const favouriteJoke = jokes.find((joke) => joke.id === id);
+    // sets the joke in favouriteJoke state
+    setFavouriteJokes([favouriteJoke, ...favouriteJokes]);
+  }
+
+  const unlike = (id) => {
+    // updates the facouriteJoke state to array without unliked joke
+    const updatedFavouriteJokes = favouriteJokes.filter((joke) => joke.id !== id);
+    // sets new state
+    setFavouriteJokes(updatedFavouriteJokes);
   }
 
   useEffect(() => {
+    // do nothing if already 10 favourite jokes
     if(stateRef.current.length > 9) return;
+    // if we click the button to start fetching
     if(buttonValue) {
+      // sets interval to start retrieving jokes
       const interval = setInterval(() => {
-        setChangeInInterval(true)
-        setIntervalId(interval)
+        //sets state to inform that interval is going
+        setChangeInInterval(true);
+        // sets the interval id in the state
+        setIntervalId(interval);
+        // fetches one random joke
         fetch('http://api.icndb.com/jokes/random/1')
           .then(res => res.json())
-          .then(res => setFavouriteJokes(favouriteJokes => ([...res.value, ...favouriteJokes])))
+          // sets new favourite joke state with a new joke
+          .then(res => setFavouriteJokes(favouriteJokes => ([...res.value, ...favouriteJokes])));
+          // if we have enough jokes
           if(stateRef.current.length > 8) {
-            setChangeInInterval(false)
-            setButtonValue(!toggle.current)
+            // sets state to inform that we need to clear interval
+            setChangeInInterval(false);
+            // changes the button value to false to inform that we stopped fetching
+            setButtonValue(!toggle.current);
           }
       }, 5000)
     }
     if(!buttonValue ) {
-      setChangeInInterval(false)
+      // if we click off the button, inform the state to clear Interval
+      setChangeInInterval(false);
     }
   }, [buttonValue])
 
   useEffect(() => {
     if(!changeInInterval) {
-      clearInterval(intervalId)
-      setIntervalId(null)
+      // if we were informed that the interval needs to be cleared, clear it and sets interval id to null
+      clearInterval(intervalId);
+      setIntervalId(null);
     }
   }, [changeInInterval, intervalId])
 
-  const fetchRandomJokes = () => {
-    setButtonValue(!toggle.current)
-  }
-
-  const setFavourite = (id) => {
-    if(favouriteJokes.length > 9 || favouriteJokes.find((joke) => joke.id === id)) return;
-    const favouriteJoke = jokes.find((joke) => joke.id === id);
-    setFavouriteJokes([favouriteJoke, ...favouriteJokes])
-  }
-
-  const unlike = (id) => {
-    const updatedFavouriteJokes = favouriteJokes.filter((joke) => joke.id !== id);
-    setFavouriteJokes(updatedFavouriteJokes);
-  }
-
-  const changeTabs = (event, value) => {
-    setCurrentTab(value)
-  }
-
   useEffect(() => {
-    window.localStorage.setItem('favouriteJokes', JSON.stringify(favouriteJokes))
+    // set favourite jokes into local storage on any change to the array
+    window.localStorage.setItem('favouriteJokes', JSON.stringify(favouriteJokes));
   }, [favouriteJokes])
 
 
